68915dfb965d3ef891ed55e541f8ffbf0fc39e94
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2022-04-04 19:16:25 +0200

    refactor and optimization of bicount

--- bigram_model/bicount.py
+++ bigram_model/bicount.py
@@ -18,28 +18,40 @@
 # THE USE OR PERFORMANCE OF THIS SOFTWARE.
 ##
 
-from sys import stdin, stdout, argv, exit
+import marshal
+import logging
+import sys
 from bisort import normalize
 from collections import Counter
 from math import log
 from string import ascii_lowercase
 from itertools import product
-import marshal
 
-if len(argv) != 1:
-    exit("Usage: {} < source.txt > model.bin".format(argv[0]))
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO, datefmt='%H:%M:%S')
+logger = logging.getLogger(__name__)
+LOG_EVERY=1000000
+
+if len(sys.argv) != 1:
+    sys.exit("Usage: {} < source.txt > model.bin".format(sys.argv[0]))
 
 total = 0
 counts = Counter()
-for line in stdin.readlines():
+
+logger.info(f"Loading data, will report every {LOG_EVERY} lines")
+for i, line in enumerate(sys.stdin):
     text = normalize(line)
     if text:
         total += len(text) - 1
         counts += Counter("".join(bigram) for bigram in zip(text, text[1:]))
 
+    if i > 0 and i % LOG_EVERY == 0:
+        logger.info(f"{i} sentences loaded")
+
+
+logger.info(f"Data loading done. Read {total} characters")
 statistics = {bigram: log(total/count) for bigram, count in counts.items()}
 inf = max(count for count in statistics.values())
 defaults = {"".join(bigram): inf for bigram in product(*2*[ascii_lowercase])}
 complete_statistics = {**defaults, **statistics}
 
-stdout.buffer.write(marshal.dumps(complete_statistics))
+sys.stdout.buffer.write(marshal.dumps(complete_statistics))
