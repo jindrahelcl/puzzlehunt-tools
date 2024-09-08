c1364587b14a878d45bc7d86641a2f2b07251112
Tomas Tintera <trosos@seznam.cz>
2024-09-08 22:28:58 +0200

    Remove the Moire effect by aligning triangle size with the dash pattern period.

--- trihex/trihex.py
+++ trihex/trihex.py
@@ -93,7 +93,7 @@ def gen_content_stream(radius, size, width, height, bleed, mark):
 
     content_stream.append(".7 G")  # Set stroke color
     content_stream.append(".5 w")  # Set line width
-    content_stream.append("[.5 1] 0 d")  # Set dash pattern
+    content_stream.append("[.5 1] .25 d")  # Set dash pattern
     dual_commands = gen_triangular_dual(hex_centers, radius)
     content_stream.extend(dual_commands)
 
@@ -116,7 +116,7 @@ def gen_pdf(radius, size, width, height, bleed, mark):
     objects.append("1 0 obj\n<< /Type /Catalog /Pages 2 0 R >>\nendobj")
     objects.append("2 0 obj\n<< /Type /Pages /Count 1 /Kids [3 0 R] /Resources << >> >>\nendobj")
     objects.append(f"3 0 obj\n<< /Type /Page /Parent 2 0 R /MediaBox [{-bleed-mark} {-bleed-mark} {width+bleed+mark} {height+bleed+mark}] /CropBox [0 0 {width} {height}] /BleedBox [{-bleed} {-bleed} {width+bleed} {height+bleed}] /Contents 4 0 R >>\nendobj")
-    #pdf_content.append(f"3 0 obj\n<< /Type /Page /Parent 2 0 R /MediaBox [{-bleed-mark-9} {-bleed-mark-9} {width+bleed+mark+9} {height+bleed+mark+9}] /Contents 4 0 R >>\nendobj")
+    #objects.append(f"3 0 obj\n<< /Type /Page /Parent 2 0 R /MediaBox [{-bleed-mark-9} {-bleed-mark-9} {width+bleed+mark+9} {height+bleed+mark+9}] /Contents 4 0 R >>\nendobj")
 
     # Content stream
     content_stream = gen_content_stream(radius, size, width, height, bleed, mark)
@@ -145,8 +145,8 @@ def gen_pdf(radius, size, width, height, bleed, mark):
     return "\n".join(pdf_content) + "\n"
 
 # Parameters
-hex_radius = 41  # Number of hexagons from center to edge in grid
-hex_size = 8   # Size of each hexagon
+hex_radius = 42  # Number of hexagons from center to edge in grid
+hex_size = 4.5*math.sqrt(3)   # Size of each hexagon
 page_width = 595  # Page width in points (8.5 inches)
 page_height = 842  # Page height in points (11 inches)
 bleed = 9
